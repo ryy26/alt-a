4356f8bd436cf2f2f532803a62e8db90333c9998
ryy26 <138737653+ryy26@users.noreply.github.com>
2024-08-27 08:08:02 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'SOMETHING FOR YOU :D'.split('')
+const text = 'SOMETHING FOR YOU:D'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
